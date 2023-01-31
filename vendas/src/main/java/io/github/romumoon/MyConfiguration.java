a5b6954041fcc9e8b5f4c8a1c69b723b76fd5385
Romumoon <romu.pastore@gmail.com>
2023-01-31 18:32:35 -0300

    Testing multiple profiles

--- vendas/src/main/java/io/github/romumoon/MyConfiguration.java
+++ vendas/src/main/java/io/github/romumoon/MyConfiguration.java
@@ -1,14 +1,23 @@
 package io.github.romumoon;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("devlopment")
 public class MyConfiguration {
 	
 	@Bean(name = "applicationName")
 	public String applicationName() {
 		return "Sistema de vendas";
 	}
-
+	
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("Running devlopment configurations.");
+		};
+	}
 }
